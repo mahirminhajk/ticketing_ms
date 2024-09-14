0f5bc368210e41e0fe6930eeefc21cd3e175f4bf
mahirminhajk <kmahirminhaj@gmail.com>
2024-09-14 17:33:14 +0530

    update: currentUser router done

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,21 @@
-import {Router} from 'express';
+import { NextFunction, Request, Response, Router } from "express";
+import jwt from "jsonwebtoken";
 
 const router = Router();
 
-router.get('/users/currentuser', (req, res) => {
-    res.send("hi");
-});
+router.get(
+  "/users/currentuser",
+  (req: Request, res: Response, next: NextFunction) => {
+    //* check session and session.jwt avaiable
+    if (!req.session?.jwt) return res.json({ currentUser: null });
+
+    try {
+      const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+      res.json({ currentUser: payload });
+    } catch (error) {
+      res.json({ currentUser: null });
+    }
+  }
+);
 
 export default router;
