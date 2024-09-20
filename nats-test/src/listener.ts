5da5b91010c98769bd94df927be976d1efc8257e
mahirminhajk <kmahirminhaj@gmail.com>
2024-09-20 23:15:24 +0530

    update: nats-test to class structure

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, {Message} from "node-nats-streaming";
+import nats from "node-nats-streaming";
 import { randomBytes } from "node:crypto";
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 
 //? Clear the console before running the script
 console.clear();
@@ -14,29 +15,13 @@ stan.on('connect', () => {
   stan.on("close", () => {
     console.log("NATS connection closed");
     process.exit();
-  }); //? This is to handle the close event
+  }); //? This is to handle the close event 
 
-  const options = stan.subscriptionOptions()
-    .setManualAckMode(true) //? This is to make sure that the message is not automatically acknowledged(we will do it manually)
-    .setDeliverAllAvailable() //? This is to make sure that the listener gets all the events that were missed while the listener was down
-    .setDurableName("order-service"); //? This is to make sure that the listener gets all the events that were missed while the listener was down
-    
-  const subscription = stan.subscribe(
-    "ticket:created",
-    "queue-group-name",
-    options
-  );
-  subscription.on("message", (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === "string") {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-    }
-
-    msg.ack(); //? This is to acknowledge
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 
 process.on("SIGINT", () => stan.close());
 process.on("SIGTERM", () => stan.close());
+
+
