0ae94d0c9201818e2bd1bd673375d4ff4471c4ca
mahirminhajk <kmahirminhaj@gmail.com>
2024-11-23 13:24:15 +0530

    update: order - expiration complete event

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -4,6 +4,7 @@ import { natsWrapper } from "./nats-wrapper";
 import {
   TicketCreatedListener,
   TicketUpdatedListener,
+  ExpirationCompleteListener,
 } from "./events/listeners";
 
 //* connect to mongodb
@@ -44,6 +45,7 @@ const startServer = async () => {
 
     new TicketCreatedListener(natsWrapper.client).listen();
     new TicketUpdatedListener(natsWrapper.client).listen();
+    new ExpirationCompleteListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to Auth mongodb");
